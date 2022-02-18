1fbbaecfa524f947bd287e5ce4aceaf68646c77b
Araxielfenix <araxielfenix@outlook.com>
2022-02-18 13:41:47 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+let contador;
+let Nombre;
+let Sprite;
 window.onload = function() {
-  document.getElementById("premio").style.display = "none";
+    hide();
+    for (let i = 1; i <= 17; i++) {
+        // Get the element
+        var elem = document.querySelector('#Ball');
+        var clone = elem.cloneNode(true);
+        clone.id = 'elem2'; 
+        clone.classList.add('text-large');
+        // Inject it into the DOM
+        elem.after(clone); 
+        contador = i;
+    }
+    getName(); 
 };
 //Al momento de pasar el mouse por encima del boton, oscurecer el color del boton.
 function oscurecer() {
@@ -12,28 +26,36 @@ function mostrar() {
 }
 //Funcion que se ejecuta al pulsar el boton.
 function myMove() {
+    document.getElementById("Pokemon").src = "";
+    if(contador > 0){
+        removeImage("elem2");
+        contador--;
+    }
+    else{
+        document.getElementById("Ball").style.display = "none";
+    }
     document.getElementById("premio").style.display = "block";
     let id = null;
     const elem = document.getElementById("premio");   
-    let pos = 500;
+    let pos = 510;
     clearInterval(id);
     id = setInterval(frame, 5);
     function frame() {
-      if (pos == 550) {
+      if (pos == 570) {
         clearInterval(id);
       } else {
         pos++; 
         elem.style.top = pos + "px";  
       }
     }
-}
-//Onclick release button hide the pokemon div.
-function hide() {
-    document.getElementById("premio").style.display = "none";
+    getName();  
+    setTimeout(hide, 1700);
+    //Asigna la imagen al div pokemon.
+    document.getElementById("Pokemon").src = Sprite;
+    Animation = setInterval(function(){document.getElementById("pokemon").style.animation = "pokemon-animation 1s";}, 1500);
 }
 //Funcion para refrescar la pagina al presionar el boton.
 function refresh() {
-    document.getElementById("premio").style.display = "none";
     location.reload();
 }
 //Al momento de pasar el mouse por encima del boton, oscurecer el color del boton.
@@ -44,4 +66,24 @@ function oscurecerF5() {
 //Al momento de quitar el mouse del boton, mostrar el color original del boton.
 function mostrarF5() {
     document.getElementById("F5").style.backgroundColor = "#2174e0";
+}
+function removeImage(id) {
+    var elementToBeRemoved = document.getElementById(id);
+    elementToBeRemoved.parentNode.removeChild(elementToBeRemoved);
+}
+//Onclick release button hide the pokemon div.
+function hide() {
+    document.getElementById("premio").style.display = "none";
+}
+//Funcion que obtiene el nombre del pokemon y lo guarda en la variable Nombre.
+function getName() {
+    //Realiza un random entre 1 y 151.
+    let random = Math.floor(Math.random() * 151) + 1;
+    Nombre = "https://pokeapi.co/api/v2/pokemon/" + random;
+    //Realiza una peticion a la API para obtener el nombre del pokemon y lo guarda en la variable Nombre.
+    fetch(Nombre)
+    .then(function(response) {
+        return response.json();
+    })
+    Sprite = "https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/" + random + ".svg";
 }
